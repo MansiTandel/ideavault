af7d6d7497117d101f0b10fcdd59880c644899e6
Mansi Tandel <97576290+MansiTandel@users.noreply.github.com>
2023-07-01 21:18:52 -0700

    Update settings.py

--- ideavault/settings.py
+++ ideavault/settings.py
@@ -36,9 +36,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ideavault-production-58fc.up.railway.app']
 
-#CSRF_TRUSTED_ORIGINS = ['https://']
+#CSRF_TRUSTED_ORIGINS = ['https://ideavault-production-58fc.up.railway.app']
 
 
 
